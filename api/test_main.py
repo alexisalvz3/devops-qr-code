b165115ddec595102aa0ce72ccb9d011b95d762d
alexisalvz3 <alexis.alvz3@gmail.com>
2025-03-20 19:00:51 -0700

    think I fixed client and new requirement

--- api/test_main.py
+++ api/test_main.py
@@ -4,7 +4,7 @@ from fastapi.testclient import TestClient
 from main import app
 
 # creating a TestClient instance.
-client = TestClient(app=app)
+client = TestClient(app)
 
 class TestQRCodeAPI:
     def test_generate_qr_code(self):
